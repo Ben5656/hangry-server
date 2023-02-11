3903933e3e008615801821d7db7cc7a81f7b2a13
Matthew Jones <matthewbtjones2000@gmail.com>
2023-02-11 15:04:17 +0000

    Add serialization dependency

--- src/main/kotlin/com/hangry/plugins/Routing.kt
+++ src/main/kotlin/com/hangry/plugins/Routing.kt
@@ -1,13 +1,22 @@
 package com.hangry.plugins
 
+import com.hangry.models.*
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 
 fun Application.configureRouting() {
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        post("/create") {
+            val (type, location, radius) = call.receive<CreateSessionBody>()
+            val sessionCode = Session.generateCode()
+            val session = Session(sessionCode, type, location, radius)
+            sessionStorage.add(session)
+            call.respond { session }
         }
     }
 }
